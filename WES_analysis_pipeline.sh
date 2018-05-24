c4bf46c451c6eb64ef5007ddb9fa5a3a75ef3775
liannesuurenbroek <39589187+liannesuurenbroek@users.noreply.github.com>
2018-05-24 14:18:15 +0200

    Update pipeline
    Foutjes verbeterd
    Volgorde sorting en marking duplicates omgedraaid

--- WES_analysis_pipeline.sh
+++ WES_analysis_pipeline.sh
@@ -6,27 +6,27 @@ bwa mem /home/ubuntu/Notebooks/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GR
 # Create BAM file
 sambamba view -S -f bam na12878_wes.sam > na12878_wes.bam
 
-# Mark duplicates
-sambamba markdupe na12878_wgs.sorted.bam dedupped_na12878_wes.sorted.bam
-
 # Sort the BAM file
 sambamba sort na12878_wes.bam
 
+# Mark duplicates
+sambamba markdupe na12878_wes.sorted.bam dedupped_na12878_wes.sorted.bam
+
 # Add readgroups
 java -jar $PICARD AddOrReplaceReadGroups \
-I=$inputfile dedupped_na12878_wes.sorted.bam \
-O=$outputfile RG_dedupped_na12878_wes.sorted.bam \
+I=dedupped_na12878_wes.sorted.bam \
+O=RG_dedupped_na12878_wes.sorted.bam \
 CREATE_INDEX=true \
-RGLB=$LB WES \
-RGPL=$PL illumina \
-RGSM=$SM na12878 \
-RGPU=$PU slide_barcode
+RGLB=WES \
+RGPL=illumina \
+RGSM=na12878 \
+RGPU=slide_barcode
 
 # Call variants
 java -jar $GATK \
 -T HaplotypeCaller \
 -R Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta  \
--I  RG_dedupped_na12878_wes.sorted.bam \
+-I RG_dedupped_na12878_wes.sorted.bam \
 -o RG_dedupped_na12878_wes.sorted.vcf
 
 # --------------------------------
